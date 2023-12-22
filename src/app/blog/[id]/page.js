3fb499cd18fa7b784a1aacd0a1d988fa2aa0d859
phuctai15794 <phuctai15794@gmail.com>
2023-12-22 17:27:54 +0700

    update blod page

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -1,15 +1,34 @@
-export default function PageDetail({ params }) {
-	console.log(params);
-	return <div>Blog detail</div>;
+// 'use client';
+import Link from 'next/link';
+import Container from 'react-bootstrap/Container';
+
+async function getPostDetail(id) {
+	const data = await fetch(`https://dummyjson.com/posts/${id}`)
+		.then((res) => res.json())
+		.then((data) => data);
+	return data;
 }
 
-export async function generateStaticParams(props) {
+export default async function PageDetail({ params }) {
+	const { id } = params;
+	const blogDetail = await getPostDetail(id);
+
+	return (
+		<Container>
+			<Link href="/blog">Back</Link>
+			<h1>{blogDetail.title}</h1>
+			{blogDetail.body}
+		</Container>
+	);
+}
+
+export async function generateStaticParams() {
 	const data = await fetch('https://dummyjson.com/posts')
 		.then((res) => res.json())
 		.then((data) => data);
 
 	return data.posts.map((post) => ({
-		id: post.id,
+		id: post.id.toString(),
 		title: post.title,
 		body: post.body
 	}));
